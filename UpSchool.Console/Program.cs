1a3069b365f11331c13b41d00a47e71cf0989239
Hanife <hanife.sayilir@cfn.com.tr>
2023-01-29 15:52:25 +0300

    init

--- UpSchool.Console/Program.cs
+++ UpSchool.Console/Program.cs
@@ -1,7 +1,8 @@
 ﻿using UpSchool.Console.Domain;
+using UpSchool.Console.Enums;
 using UpSchool.Console.FirstExample;
 
-Student student = new Student();
+/*Student student = new Student();
 
 student.FirstName = "Alper";
 
@@ -32,5 +33,40 @@ if (colour == Colour.Red)
 //Console.WriteLine(student.FinalNotes);
 
 Console.WriteLine(student.FullName);
+
+Console.ReadLine();*/
+
+const string filePath = $@"C:\Users\hanif\OneDrive\Masaüstü\\Access_Control_Logs.txt";
+
+var logsText = File.ReadAllText(filePath);
+
+
+var splitLines = logsText.Split('\n',StringSplitOptions.RemoveEmptyEntries);
+
+splitLines.ToList().ForEach(line => Console.WriteLine(line));
+
+var logs = new List<AccessControlLog>();
+
+foreach (var line in splitLines.Skip(1))
+{
+    var logFields = line.Replace(" ",string.Empty)
+        .Replace("\r",string.Empty)
+        .Split("---",StringSplitOptions.RemoveEmptyEntries);
+    var accessControlLog = new AccessControlLog()
+    {
+        UserId = Convert.ToInt32(logFields[0]),
+        DeviceSerialNo = logFields[1],
+        AccessType = AccessControlLog.ConvertToAccessType(logFields[2]),
+        Date = Convert.ToDateTime(logFields[3]),
+    };
+    logs.Add(accessControlLog);
+}
+var cardLogs = logs.Where(x => x.AccessType == AccessType.CARD)
+    .Where(x => x.DeviceSerialNo == "X01X2500S")
+    .ToList();
+
+cardLogs.Count();
+
+cardLogs.ForEach(log => Console.WriteLine($"{log.UserId} - {log.DeviceSerialNo} - {log.AccessType} - {log.Date}"));
 
 Console.ReadLine();
